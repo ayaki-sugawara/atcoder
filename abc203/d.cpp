d9be3d344fe5be50fd6df292cc7d4c26dbef1c94
ayaki-sugawara <ayaki.s.1015@keio.jp>
2021-11-26 00:59:36 +0900

    add d

---
+++ abc203/d.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int k, n;
+  vector<vector<int>> a(n, vector<int>(n));
+  for (int i=0; i<n; i++){
+    cin >> 
+  }
+  return 0;
+}
